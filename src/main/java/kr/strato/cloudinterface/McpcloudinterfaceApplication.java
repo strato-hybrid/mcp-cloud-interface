0571d17d41f44160d988bbbafb6d7a0674286564
as-shpark <as-shpark@kakao.com>
2022-10-18 08:52:49 +0900

    init porject

---
+++ src/main/java/kr/strato/cloudinterface/McpcloudinterfaceApplication.java
@@ -0,0 +1,14 @@
+package kr.strato.cloudinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class McpcloudinterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpcloudinterfaceApplication.class, args);
+	}
+
+}
