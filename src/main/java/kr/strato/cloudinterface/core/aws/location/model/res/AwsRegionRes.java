ea369d780ab904fe5e300e435ee276fd8d0537f9
strato128 <114896245+starto128@users.noreply.github.com>
2023-02-27 16:39:40 +0900

    Update AwsRegionRes.java

--- src/main/java/kr/strato/cloudinterface/core/aws/location/model/res/AwsRegionRes.java
+++ src/main/java/kr/strato/cloudinterface/core/aws/location/model/res/AwsRegionRes.java
@@ -2,10 +2,7 @@ package kr.strato.cloudinterface.core.aws.location.model.res;
 
 import lombok.*;
 
-/**
- * AWS 리전 조회 객체
- * sample: endPoint=ec2.eu-north-1.amazonaws.com, regionName=eu-north-1, optInStatus=opt-in-not-required
- */
+
 @Getter
 @Setter
 @NoArgsConstructor
